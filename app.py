d5bce3c83df5102061bb281e7c4cc7a79cb7469b
Matias-Pepe <m.pepe@itecriocuarto.org.ar>
2025-05-13 19:15:14 -0300

    get api y ciudades activas

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
+import requests
 from datetime import date
-
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+
+#Ciudades
+ciudades = {
+    "Buenos Aires": {"lat": -34.6037, "lon": -58.3816},
+    "Córdoba": {"lat": -31.4201, "lon": -64.1888},
+    "Madrid": {"lat": 40.4168, "lon": -3.7038},
+    "Nueva York": {"lat": 40.7128, "lon": -74.0060},
+    "Tokio": {"lat": 35.6895, "lon": 139.6917},
+    "París": {"lat": 48.8566, "lon": 2.3522},
+    "Londres": {"lat": 51.5074, "lon": -0.1278},
+    "Sídney": {"lat": -33.8688, "lon": 151.2093},
+    "Ciudad de México": {"lat": 19.4326, "lon": -99.1332},
+    "El Cairo": {"lat": 30.0444, "lon": 31.2357}
+}
+
 
 @app.route('/')
 def index():
     return render_template(
-        'index.html'
+        'index.html',
+        ciudades=ciudades.keys()
     )
 
 @app.route('/clima')
 def clima():
+    ciudad = request.args.get('ciudad', '').lower()
+    lat = ciudades[ciudad]['lat']
+    lon = ciudades[ciudad]['lon']
+    response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=-31.42&longitude=-64.18&current_weather=true")
+    data = response.json()
     return render_template(
         'clima.html',
-        fecha=date.today().strftime("%d/%m/%Y")
+        fecha=date.today().strftime("%d/%m/%Y"),
+        clima=clima['current_weather']
     )
 
